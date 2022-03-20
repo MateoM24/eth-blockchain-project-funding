75e069d732416f014e2809e8a24f4f2d02ca7804
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2022-03-20 14:07:37 +0000

    solidity contracts and compiling script

---
+++ ethereum/compile.js
@@ -0,0 +1,42 @@
+import path, { dirname } from "path";
+import solc from "solc";
+import fs from "fs-extra";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+const output = solc.compile(
+  JSON.stringify({
+    language: "Solidity",
+    sources: {
+      "Campaign.sol": {
+        content: source,
+      },
+    },
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["*"],
+        },
+      },
+    },
+  })
+);
+
+fs.ensureDirSync(buildPath);
+const contracts = JSON.parse(output).contracts["Campaign.sol"];
+// console.log(JSON.parse(output));
+for (let contractName in contracts) {
+  //   console.log(contracts[contractName].abi);
+  console.log(path.resolve(buildPath, contractName + ".json"));
+  fs.outputJsonSync(
+    path.resolve(buildPath, contractName + ".json"),
+    contracts[contractName].abi
+  );
+}
