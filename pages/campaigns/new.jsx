89a31d3809f498b57ee89a417a561f701ee14adc
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2022-04-09 20:51:57 +0100

    simple input error handling

--- pages/campaigns/new.jsx
+++ pages/campaigns/new.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
-import { Form, Button, Input } from "semantic-ui-react";
-
+import { Form, Button, Input, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 export default () => {
   const [minContribution, setMinContribution] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(minContribution).send({
+        // gas doesn't need to be specified here as metamask has a funcionality to calculate expected gas amount on its own.
+        from: accounts[0],
+      });
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
   return (
     <Layout>
       <h1>Create a Campaign</h1>
 
-      <Form>
+      <Form onSubmit={onSubmit} error={!!errorMessage}>
         <Form.Field>
           <label>Minimum Contribution</label>
           <Input
@@ -19,6 +36,8 @@ export default () => {
             onChange={(event) => setMinContribution(event.target.value)}
           />
         </Form.Field>
+
+        <Message error header="Oops!" content={errorMessage} />
         <Button primary>Create!</Button>
       </Form>
     </Layout>
