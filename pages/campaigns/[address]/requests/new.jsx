4889ba9e371a7bb846039e1fcf99b39cac37dbe7
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2022-04-26 22:17:09 +0100

    create request form UI and logic

--- pages/campaigns/[address]/requests/new.jsx
+++ pages/campaigns/[address]/requests/new.jsx
@@ -1,3 +1,61 @@
-export default () => {
-  return <div>new request</div>;
+import { Input, Button, Message, Form } from "semantic-ui-react";
+import Campaign from "../../../../ethereum/campaign";
+import web3 from "../../../../ethereum/web3";
+import Link from "next/link";
+import { useState } from "react";
+import Layout from "../../../../components/Layout";
+
+export default (props) => {
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [recipient, setRecipient] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const campaign = Campaign(props.address);
+      await campaign.methods
+        .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
+        .send({ from: accounts[0] });
+    } catch (err) {}
+  };
+
+  return (
+    <Layout>
+      <h3>Create a Request</h3>
+      <Form onSubmit={onSubmit}>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Value in Ether</label>
+          <Input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={recipient}
+            onChange={(event) => setRecipient(event.target.value)}
+          />
+        </Form.Field>
+        <Button primary>Create</Button>
+      </Form>
+    </Layout>
+  );
 };
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      address: context.query.address,
+    },
+  };
+}
