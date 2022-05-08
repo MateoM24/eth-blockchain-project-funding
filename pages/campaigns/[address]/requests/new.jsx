ce89a655b13abfe253da8c78035163c02ee3c5f3
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2022-05-08 18:27:32 +0100

    add back link to all requests from add request form; add error and loading state handling

--- pages/campaigns/[address]/requests/new.jsx
+++ pages/campaigns/[address]/requests/new.jsx
@@ -3,28 +3,43 @@ import Campaign from "../../../../ethereum/campaign";
 import web3 from "../../../../ethereum/web3";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Layout from "../../../../components/Layout";
 
 export default (props) => {
   const [description, setDescription] = useState("");
   const [value, setValue] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const router = useRouter();
 
   const onSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
+    setErrorMessage("");
     try {
       const accounts = await web3.eth.getAccounts();
       const campaign = Campaign(props.address);
       await campaign.methods
         .createRequest(description, web3.utils.toWei(value, "ether"), recipient)
         .send({ from: accounts[0] });
-    } catch (err) {}
+      router.push(`/campaigns/${props.address}/requests`);
+    } catch (err) {
+      setErrorMessage(err.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <Layout>
+      <Link href={`/campaigns/${props.address}/requests`}>
+        <a>Back</a>
+      </Link>
       <h3>Create a Request</h3>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} error={!!errorMessage}>
         <Form.Field>
           <label>Description</label>
           <Input
@@ -46,7 +61,11 @@ export default (props) => {
             onChange={(event) => setRecipient(event.target.value)}
           />
         </Form.Field>
-        <Button primary>Create</Button>
+
+        <Message error header="Oops!" content={errorMessage} />
+        <Button primary loading={loading}>
+          Create
+        </Button>
       </Form>
     </Layout>
   );
